7ddbb1fbac3e24b79ead802ce6e0f7a49863a734
Tom Haines <tom@tomhaines.xyz>
2020-11-17 14:48:09 +0000

    Boilerplate a react app and api endpoint

---
+++ firmware/frontend/src/components/Root/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Stream from '../Stream'
+
+const Root = () => {
+  return (
+    <>
+    <h1>Ocean Gromits</h1>
+    <Stream />
+    </>
+  )
+}
+
+export default Root
---
+++ firmware/frontend/src/components/Stream/index.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react'
+
+const Stream = () => {
+  let [peer, setPeer] = useState({})
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+      <p>Stream container</p>
+  )
+}
+
+export default Stream
