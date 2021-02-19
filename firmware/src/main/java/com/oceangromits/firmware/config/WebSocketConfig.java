796e539fffafe2879fd009e6ceaf20f27a49b322
zapydoodle <you@example.com>
2021-02-19 14:18:47 +0000

    First steps of server side authentication

--- firmware/src/main/java/com/oceangromits/firmware/config/WebSocketConfig.java
+++ firmware/src/main/java/com/oceangromits/firmware/config/WebSocketConfig.java
@@ -1,8 +1,32 @@
 package com.oceangromits.firmware.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.Collections;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,3 +43,102 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
 }
+
+
+//code modified from https://stackoverflow.com/questions/45405332/websocket-authentication-and-authorization-in-spring
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/signaller").permitAll()
+                .anyRequest().denyAll();
+    }
+}
+
+//authenticates a user, returns a UsernamePasswordAuthenticationToken
+@Component
+class WebSocketAuthenticatorService {
+    // This method MUST return a UsernamePasswordAuthenticationToken instance, the spring security chain is testing it with 'instanceof' later on. So don't use a subclass of it or any other class
+    public UsernamePasswordAuthenticationToken getAuthenticatedOrFail(final String username, final String password) throws AuthenticationException {
+        if (username == null || username.trim().isEmpty()) {
+            throw new AuthenticationCredentialsNotFoundException("Username was null or empty.");
+        }
+        if (password == null || password.trim().isEmpty()) {
+            throw new AuthenticationCredentialsNotFoundException("Password was null or empty.");
+        }
+        // If we End up using a database for username/password
+        //if (fetchUserFromDb(username, password) == null) {
+        //throw new BadCredentialsException("Bad credentials for user " + username);
+        //}
+
+        // null credentials, we do not pass the password along
+        return new UsernamePasswordAuthenticationToken(
+                username,
+                null,
+                Collections.singleton((GrantedAuthority) () -> "USER") // MUST provide at least one role
+        );
+    }
+}
+
+@Component
+class AuthChannelInterceptorAdapter implements ChannelInterceptor {
+    private static final String USERNAME_HEADER = "login";
+    private static final String PASSWORD_HEADER = "passcode";
+    private final WebSocketAuthenticatorService webSocketAuthenticatorService;
+    @Inject
+    public AuthChannelInterceptorAdapter(final WebSocketAuthenticatorService webSocketAuthenticatorService) {
+        this.webSocketAuthenticatorService = webSocketAuthenticatorService;
+    }
+
+    @Override
+    public Message<?> preSend(final Message<?> message, final MessageChannel channel) throws AuthenticationException {
+        final StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+            final String username = accessor.getFirstNativeHeader(USERNAME_HEADER);
+            final String password = accessor.getFirstNativeHeader(PASSWORD_HEADER);
+
+            final UsernamePasswordAuthenticationToken user = webSocketAuthenticatorService.getAuthenticatedOrFail(username, password);
+
+            accessor.setUser(user);
+        }
+        return message;
+    }
+}
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)//allows the interceptor to be registered first
+class WebSocketAuthenticationSecurityConfig implements WebSocketMessageBrokerConfigurer {
+    @Inject
+    private AuthChannelInterceptorAdapter authChannelInterceptorAdapter;
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        // Endpoints are already registered on WebSocketConfig, no need to add more.
+    }
+
+    @Override
+    public void configureClientInboundChannel(final ChannelRegistration registration) {
+        registration.interceptors(authChannelInterceptorAdapter);
+    }
+
+}
+
+@Configuration
+class WebSocketAuthorizationSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+    @Override
+    protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
+        // You can customize your authorization mapping here.
+        messages.anyMessage().authenticated();
+    }
+
+    // TODO: For test purpose (and simplicity) i disabled CSRF, but you should re-enable this and provide a CRSF endpoint.
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
+
+    
