a0e0c797aa3ced9504952cb11f55cadc870a4c7a
Ricky Pinder <pinderrr@outlook.com>
2021-03-19 10:50:53 +0000

    created smartdata component, no current content

--- firmware/frontend/src/components/Root/index.js
+++ firmware/frontend/src/components/Root/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Stream from '../Stream'
 import SignalProvider from "../SignalProvider";
+import SmartData from '../SmartData/index.js'
 
 const signalServerPath = process.env.REACT_APP_SIGNAL_URL  || "http://localhost:8080"
 
 const Root = () => {
-  return (
+  return <>
     <SignalProvider url={signalServerPath + "/signaller"}>
       <Stream />
     </SignalProvider>
-  )
+    <SmartData />
+  </>
 }
 
 export default Root
---
+++ firmware/frontend/src/components/SmartData/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SmartData.module.scss'
+
+const SmartData = () => {
+    return <div className={styles.smartData}>
+        <p>TEXT</p>
+        </div>
+}
+
+export default SmartData
