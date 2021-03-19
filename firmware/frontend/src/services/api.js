e4ee602b2b2a3ca7f5fe1ed32583022fc80c259b
Tom Haines <tom@tomhaines.xyz>
2021-03-19 16:35:43 +0000

    Build API Hook

--- firmware/frontend/src/services/api.js
+++ firmware/frontend/src/services/api.js
@@ -1,11 +1,36 @@
+import {useEffect, useState } from 'react'
 import useToken from "./token";
+import url from './url'
 
-const useApi = (method, ...params) => {
+const useResource = (query, ...params) => {
   const [data, setData]           = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const [error, setError]         = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
 
-  const [token, setToken] = useToken()
+  const [token] = useToken();
 
+  useEffect(() => {
+    const fetchApi = async () => {
+      setIsLoading(true)
+      try {
+        const res = await fetch(url + query, {
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        })
+
+        const json = await res.json()
+        setData(json)
+        setIsLoading(false)
+      } catch (err) {
+        setError(err)
+      }
+    }
+
+    fetchApi()
+
+  }, [query, token])
+
+  return {data, error, isLoading}
 
 }
