6fbb0be01c11f07b7184eeb071ca5a35d3f5f069
Ricky Pinder <pinderrr@outlook.com>
2021-03-19 11:07:01 +0000

    passing key through to component

--- firmware/frontend/src/components/Root/index.js
+++ firmware/frontend/src/components/Root/index.js
@@ -4,13 +4,14 @@ import SignalProvider from "../SignalProvider";
 import SmartData from '../SmartData/index.js'
 
 const signalServerPath = process.env.REACT_APP_SIGNAL_URL  || "http://localhost:8080"
+const apiKey = process.env.OPENWEATHER_API_KEY
 
 const Root = () => {
   return <>
     <SignalProvider url={signalServerPath + "/signaller"}>
       <Stream />
     </SignalProvider>
-    <SmartData />
+    <SmartData api={apiKey}/>
   </>
 }
 
--- firmware/frontend/src/components/SmartData/index.js
+++ firmware/frontend/src/components/SmartData/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './SmartData.module.scss'
 
-const SmartData = () => {
+const SmartData = ({api}) => {
+    console.log(`key ${api}`)
     return <div className={styles.smartData}>
         <p>TEXT</p>
         </div>
