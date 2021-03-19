a3f7bbd145033208d23d3ad4bf50d5fe4626cf64
Ricky Pinder <pinderrr@outlook.com>
2021-03-19 12:41:25 +0000

    updates weather every 20 seconds

--- firmware/frontend/src/components/SmartData/index.js
+++ firmware/frontend/src/components/SmartData/index.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './SmartData.module.scss'
 
 // The city id for Bristol. Uisng id other name prevents conflict with other cities named Bristol
 const city = "2654675"
 
 const getWeatherData = (api, set) => {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api}&units=metric`)
         .then(res => res.json())
         .then(data => set(data))
 }
 
 const temperature = (data) => {
+    if (data == null || data.main == null) {
+        return 0
+    }
     return data.main.temp
 }
  
 const SmartData = ({api}) => {
     const [weather, setWeather] = useState(null)
 
-    getWeatherData(api, setWeather)
-    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getWeatherData(api, setWeather)
+            console.log("updating weather")
+        }, 20000)
+        return () => clearInterval(interval) 
+    }, [])
 
     return <div className={styles.smartData}>
         <p>{temperature(weather)}</p>
