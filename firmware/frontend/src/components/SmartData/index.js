70c111e0a8421944fc2821bc68252feaa4870e11
Ricky Pinder <pinderrr@outlook.com>
2021-03-19 12:13:34 +0000

    shows temperature using react state hooks

--- firmware/frontend/src/components/SmartData/index.js
+++ firmware/frontend/src/components/SmartData/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SmartData.module.scss'
 
 // The city id for Bristol. Uisng id other name prevents conflict with other cities named Bristol
 const city = "2654675"
 
-const getWeatherData = (api) => {
+const getWeatherData = (api, set) => {
         fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => set(data))
 }
 
 const temperature = (data) => {
-    console.log(data)
     return data.main.temp
 }
  
 const SmartData = ({api}) => {
-    console.log(`key ${api}`)
+    const [weather, setWeather] = useState(null)
 
-    getWeatherData(api)
+    getWeatherData(api, setWeather)
+    
 
     return <div className={styles.smartData}>
-        <p>temp</p>
+        <p>{temperature(weather)}</p>
         </div>
 }
 
