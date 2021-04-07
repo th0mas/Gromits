2478efd4a11f460bdeacf25ccdb4acafba350d55
Tom Haines <tom@tomhaines.xyz>
2021-04-07 14:16:43 +0100

    Secure websocket chans

--- firmware/src/main/java/com/oceangromits/firmware/controller/api/AdminController.java
+++ firmware/src/main/java/com/oceangromits/firmware/controller/api/AdminController.java
@@ -4,6 +4,7 @@ import com.oceangromits.firmware.model.Client;
 import com.oceangromits.firmware.repository.ClientRepository;
 import com.oceangromits.firmware.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,16 +13,26 @@ public class AdminController {
 
     private final ClientRepository clientRepository;
     private final ClientService clientService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @Autowired
-    public AdminController(ClientRepository clientRepository, ClientService clientService) {
+    public AdminController(ClientRepository clientRepository,
+                           ClientService clientService,
+                           SimpMessagingTemplate simpMessagingTemplate) {
         this.clientRepository = clientRepository;
         this.clientService = clientService;
+        this.simpMessagingTemplate = simpMessagingTemplate;
     }
 
     @PostMapping("/login")
     public String login(@RequestBody Client client) {
         return clientService.signin(client.getName(), client.getPassword());
+    }
+
+    @PostMapping("/authorize_client")
+    public Client authorizeClient(@RequestBody Client client) {
+
+        return client;
     }
 
 
