aa2367cbe0de97d1be310eeb07b32a172de40263
zapydoodle <you@example.com>
2021-04-09 13:52:12 +0100

    Added test blueprint for if there are >2 clients, unsure on how to generate a scenario for >2 clients for a test?

--- firmware/src/test/java/com/oceangromits/firmware/OceanGromitsApplicationTests.java
+++ firmware/src/test/java/com/oceangromits/firmware/OceanGromitsApplicationTests.java
@@ -1,14 +1,28 @@
 package com.oceangromits.firmware;
 
 
+import com.oceangromits.firmware.controller.SignallerController;
+import com.oceangromits.firmware.model.WebRTCSignal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class OceanGromitsApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	//tests for signal crontroller
+
+	//test whether user is connecting to a full instance (need to add paremeters to joinClient method)
+	@Test
+	public void signalControlTests(){
+		SignallerController testSignallerController = new SignallerController();
+		WebRTCSignal isfull = testSignallerController.joinClient();//not too sure on correct parameters to make it soi there's >2 clients
+		assertEquals(isfull,null);
+
+	}
+
 
 }
