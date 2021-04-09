eea74c4fc4fb26bbdb13aa071a09a288b44ebc39
Ricky Pinder <pinderrr@outlook.com>
2021-04-09 12:54:43 +0100

    refactored smartdata functions to lib to help with mocking

--- firmware/frontend/src/components/SmartData/index.js
+++ firmware/frontend/src/components/SmartData/index.js
@@ -1,23 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styles from './SmartData.module.scss'
+import {getWeatherData, temperature, getIconUrl} from '../../lib/SmartData'
 
-// The city id for Bristol. Uisng id other name prevents conflict with other cities named Bristol
-const city = "2654675"
-
-const getWeatherData = (api, set) => {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api}&units=metric`)
-        .then(res => res.json())
-        .then(data => set(data))
-}
-
-const temperature = (data) => {
-    return data.main.temp.toFixed(1)
-}
-
-const icon = (data) => {
-    return data.weather[0].icon
-}
- 
 const SmartData = ({api}) => {
     const [weather, setWeather] = useState(null)
 
@@ -41,7 +25,7 @@ const SmartData = ({api}) => {
     } else {
         return <div className={styles.smartData}>
             <p>{temperature(weather)}°C</p>
-            <img src={`http://openweathermap.org/img/wn/${icon(weather)}@2x.png`} alt='Weather Icon'/>
+            <img src={getIconUrl(weather)} alt='Weather Icon'/>
             </div>
     }
 }
