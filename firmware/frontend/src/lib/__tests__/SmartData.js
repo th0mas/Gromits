efceb80c0d024c89c014941e7f1be8fef4610910
Ricky Pinder <pinderrr@outlook.com>
2021-04-20 09:35:14 +0100

    added test for temperature of smartdata lib

--- firmware/frontend/src/lib/__tests__/SmartData.js
+++ firmware/frontend/src/lib/__tests__/SmartData.js
@@ -11,4 +11,8 @@ describe('Data filtering functions', () => {
     it ("Gets the correct icon from the weather data", () => {
         expect(getIconUrl(data)).toBe("http://openweathermap.org/img/wn/04d@2x.png")
     })
+
+    it ("Gets the correct temperature from the weather data", () => {
+        expect(temperature(data)).toBe("25.0")
+    })
 })
