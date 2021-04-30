dfda39eb7abd7952e9d58d3675ca81b79c505f1f
FlameRobin <60356880+FlameRobin@users.noreply.github.com>
2021-04-30 14:55:56 +0100

    Add recording notice to Gromit screen

--- firmware/frontend/src/components/Stream/index.js
+++ firmware/frontend/src/components/Stream/index.js
@@ -6,6 +6,7 @@ import {useVideoStream} from "../../lib/stream";
 import {SignalContext} from "../../contexts";
 import useResource from "../../services/api";
 import {SetupPromptBox} from "../InfoBox/SetupPromptBox";
+import {FilmingNotice} from "../InfoBox/FilmingNotice"
 
 const Stream = () => {
   let videoEl = useRef(null)
@@ -32,14 +33,18 @@ const Stream = () => {
   return <div className="h-screen w-screen overflow-hidden">
     <video autoPlay ref={videoEl} className="h-screen w-screen object-cover"/>
 
+
+
     <InfoHolder>
       {/* If we have any errors, render them here*/}
       {(!isLoading && !data) && <SetupPromptBox />}
       { (data && streamErr) && <InfoBox info={streamErr} />}
       { (data && err) && <InfoBox info={err} />}
-      {<InfoBox info={"Hello! Just to let you know, there is a camera in my periscope and it's currently sending a live video feed to my friend's porthole screen. If you look in my porthole, you can see what is happening where my friend is."}}
+      {<FilmingNotice />}
     </InfoHolder>
   </div>
+
+
 }
 
 export default Stream
