d23f1fb7976f286b2351d8ea303ebab9dfb15c20
Tom Haines <tom@tomhaines.xyz>
2021-05-11 13:35:15 +0100

    Change font

--- firmware/frontend/src/components/InfoBox/FilmingNotice.js
+++ firmware/frontend/src/components/InfoBox/FilmingNotice.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const FilmingNotice = () => {
     return <div className="bg-gradient-to-tr from-blue-400 to-blue-300 rounded-lg">
         <div className="p-2 flex flex-col">
-            <p className="pb-2 font-bold text-gray-100">Hello! There is a camera in my periscope sending live video to another Gromit just like me. If you look in my porthole, you can see what is happening where my friend is.</p>
+            <p className="pb-2 text-gray-100">Hello! There is a camera in my periscope sending live video to another Gromit just like me. If you look in my porthole, you can see what is happening where my friend is.</p>
         </div>
     </div>
 }
