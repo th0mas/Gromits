b51035a8f877445a4f2e48fde45bbe44e2fa06c7
Tom Haines <tom@tomhaines.xyz>
2021-05-14 14:53:33 +0100

    Add responsive login page

--- firmware/frontend/src/pages/Auth/index.js
+++ firmware/frontend/src/pages/Auth/index.js
@@ -9,8 +9,8 @@ const Admin = () => {
   let {data, isLoading} = useResource("setup/status")
 
   return (
-    <div className="h-screen w-screen bg-gradient-to-tr from-blue-400 to-green-400 flex">
-      <div className="bg-white p-4 rounded-2xl m-auto w-1/3 shadow">
+    <div className="h-screen w-screen bg-gradient-to-tr from-blue-400 to-green-400 md:flex pt-4 md:pt-0">
+      <div className="bg-white p-4 rounded-2xl m-auto w-full md:w-1/3 shadow h-full md:h-auto">
         <div className="flex space-x-2 items-center pb-4 mb-2 border-b-2">
           <img src={submarine} className="h-7 w-7" alt="Ocean Gromits logo"/>
           <p className="text-2xl font-bold">Ocean Gromits</p>
