1be367b8bf85e000ca8e88c846d25053bf11c0cd
Ricky Pinder <pinderrr@outlook.com>
2021-05-14 09:17:20 +0100

    added hasRole tests

--- firmware/frontend/src/lib/__tests__/tokenUtils.js
+++ firmware/frontend/src/lib/__tests__/tokenUtils.js
@@ -1,12 +1,22 @@
 import {decodeToken, hasRole, clientId} from "../tokenUtils"
 
-it("decodes tokens", () => {
-    const token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJncm9taXRfMmJwbWsiLCJhdXRoIjpbIlJPTEVfQ09OTkVDVCJdLCJpYXQiOjE2MjA5Nzk1MDcsImV4cCI6MTY1MjUxNTUwN30.XRVkvdmcCjzmPBPitZ0n1HwvyAWTXdEcIxgk_ilMLaQ"
-    const expected = {
-        sub: 'gromit_2bpmk',
-        auth: [ 'ROLE_CONNECT' ],
-        iat: 1620979507,
-        exp: 1652515507
-    } 
-    expect(decodeToken(token)).toStrictEqual(expected)
+describe("Token Utilities", () => {
+        const token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJncm9taXRfMmJwbWsiLCJhdXRoIjpbIlJPTEVfQ09OTkVDVCJdLCJpYXQiOjE2MjA5Nzk1MDcsImV4cCI6MTY1MjUxNTUwN30.XRVkvdmcCjzmPBPitZ0n1HwvyAWTXdEcIxgk_ilMLaQ"
+    it("decodes tokens", () => {
+        const expected = {
+            sub: 'gromit_2bpmk',
+            auth: [ 'ROLE_CONNECT' ],
+            iat: 1620979507,
+            exp: 1652515507
+        } 
+        expect(decodeToken(token)).toStrictEqual(expected)
+    })
+
+    it("Correctly finds the role in the token", () => {
+        expect(hasRole(token, 'ROLE_CONNECT'))
+    })
+
+    it("Correctly returns false when role not found", () => {
+        expect(!hasRole(token, 'admin'))
+    })
 })
